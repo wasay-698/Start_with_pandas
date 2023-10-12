f63df5170f72d77db8e05dac0022f01778a1e37d
wasay <145287518+wasay-698@users.noreply.github.com>
2023-10-12 23:19:33 +0500

    Create pandas.py

---
+++ pandas.py
@@ -0,0 +1,5 @@
+import pandas as pd
+pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
+pd.DataFrame({'Name':["This is wasay","ML student"],'skills' :["I am learning Data Science","I also have knowledge of python"]})
+pd.DataFrame({'wasay':["present",'today'],'Sami':["Absent",'today']})
+
