ff469ce33e6ee0984b9619579c410c5878d9b174
wasay <145287518+wasay-698@users.noreply.github.com>
2023-10-17 10:42:52 +0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -2,4 +2,6 @@ import pandas as pd
 pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
 pd.DataFrame({'Name':["This is wasay","ML student"],'skills' :["I am learning Data Science","I also have knowledge of python"]})
 pd.DataFrame({'wasay':["present",'today'],'Sami':["Absent",'today']})
+pd.DataFrame({'Mr.A':["I like this one","This was awosome"],'Mr.B':["I didn't like this one", "This was ugly"]},
+index = ['Vase','Spoon'])
 
