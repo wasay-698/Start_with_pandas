709091b9b0d91bacf542e895ff074057e4d7fb8b
wasay <145287518+wasay-698@users.noreply.github.com>
2024-01-07 23:50:08 +0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -5,11 +5,6 @@ pd.DataFrame({'wasay':["present",'today'],'Sami':["Absent",'today']})
 pd.DataFrame({'Mr.A':["I like this one","This was awosome"],'Mr.B':["I didn't like this one", "This was ugly"]},
 index = ['Vase','Spoon'])
 
-
-
-
-
-
 print(index)
 
 a = [1,2,3,4,5,6,7,8,9,10]
