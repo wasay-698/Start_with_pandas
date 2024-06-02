a549daa4485c48294c5cb5997809463e5284127f
wasay <145287518+wasay-698@users.noreply.github.com>
2024-06-02 15:29:02 +0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -4,15 +4,9 @@ pd.DataFrame({'Name':["This is wasay","ML student"],'skills' :["I am learning Da
 pd.DataFrame({'wasay':["present",'today'],'Sami':["Absent",'today']})
 pd.DataFrame({'Mr.A':["I like this one","This was awosome"],'Mr.B':["I didn't like this one", "This was ugly"]},
 index = ['Vase','Spoon'])
-print(index)
 print("I am back")
 print("Welcome back")
 #start with data science basics ,machine learning etc....
-a = [1,2,3,4,5,6,7,8,9,10]
-b = [4,5,6,8,9],
-c = a+b
-print(a+b)
-print(c)
 print("start your work")
 pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
 
