044710982d1caa99433a0c88dd6e5174a9a5fb8a
wasay <145287518+wasay-698@users.noreply.github.com>
2024-06-02 15:29:32 +0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
 pd.DataFrame({'Name':["This is wasay","ML student"],'skills' :["I am learning Data Science","I also have knowledge of python"]})
 pd.DataFrame({'wasay':["present",'today'],'Sami':["Absent",'today']})
