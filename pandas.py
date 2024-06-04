44a9a950ccca2ffe4957a8419d460180756e672f
wasay <145287518+wasay-698@users.noreply.github.com>
2024-06-04 13:00:49 +0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -6,6 +6,7 @@ pd.DataFrame({'wasay':["present",'today'],'Sami':["Absent",'today']})
 pd.DataFrame({'Mr.A':["I like this one","This was awosome"],'Mr.B':["I didn't like this one", "This was ugly"]},
 index = ['Vase','Spoon'])
 print("I am back")
+
 #start with data science basics ,machine learning etc....
 
 print("start your work")
