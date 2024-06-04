32ee6d631445524f0cd45a1915f14e8d623cafed
wasay <145287518+wasay-698@users.noreply.github.com>
2024-06-04 13:01:18 +0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -2,9 +2,6 @@ import pandas as pd
 import numpy as np
 pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
 pd.DataFrame({'Name':["This is wasay","ML student"],'skills' :["I am learning Data Science","I also have knowledge of python"]})
-pd.DataFrame({'wasay':["present",'today'],'Sami':["Absent",'today']})
-pd.DataFrame({'Mr.A':["I like this one","This was awosome"],'Mr.B':["I didn't like this one", "This was ugly"]},
-index = ['Vase','Spoon'])
 print("I am back")
 
 #start with data science basics ,machine learning etc....
