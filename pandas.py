28fc230b44395448a3a05de756a33691d01e22ec
wasay <145287518+wasay-698@users.noreply.github.com>
2024-06-07 20:33:05 +0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
 pd.DataFrame({'Name':["This is wasay","ML student"],'skills' :["I am learning Data Science","I also have knowledge of python"]})
-print("I am back")
+print("I am back");
+
 
 #start with data science basics ,machine learning etc....
 print("start your work")
