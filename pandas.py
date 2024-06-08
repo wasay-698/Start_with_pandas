cd550774a9388a693b0cde051cb11199b89104ca
wasay <145287518+wasay-698@users.noreply.github.com>
2024-06-08 21:13:12 +0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -5,7 +5,6 @@ pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
 pd.DataFrame({'Name':["This is wasay","ML student"],'skills' :["I am learning Data Science","I also have knowledge of python"]})
 print("I am back");
 
-
 #start with data science basics ,machine learning etc....
 print("start your work")
 
